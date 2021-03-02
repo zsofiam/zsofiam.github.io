93e098e5efe89d294f922c415fc83269020d66f5
Farkas Timot <farkas.timot@gmail.com>
2021-03-02 10:35:56 +0100

    Generate the game board

--- js/game.js
+++ js/game.js
@@ -1,1 +1,48 @@
-game.js
+const game = {
+	init: function() {
+		const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const boardHeight = parseInt(urlParams.get('height'));
+        const boardWidth = parseInt(urlParams.get('width'));
+
+        this.generateBoard(boardHeight, boardWidth);
+	},
+
+	generateBoard: function(height, width) {
+		let containerDiv = document.querySelector(".game-field");
+
+		gameField = this.addTable(containerDiv);
+
+		let cellIndex = 0;
+		for (let row = 0; row < width; row++) {
+			const rowElement = this.addRow(gameField);
+			for (let col = 0; col < height; col++) {
+				this.addCell(rowElement, row, col);
+                cellIndex++;
+			}
+		}
+	},
+
+	addTable: function(containerDiv) {
+		containerDiv.insertAdjacentHTML(
+            'beforeend',
+            '<table class="center border"><tbody></tbody></table>'
+        );
+        return containerDiv.lastElementChild.lastElementChild;
+	},
+
+	addRow: function(gameField) {
+        gameField.insertAdjacentHTML(
+            'beforeend',
+            '<tr></tr>'
+        );
+        return gameField.lastElementChild;
+    },
+    addCell: function (rowElement, row, col) {
+        rowElement.insertAdjacentHTML(
+            'beforeend',
+            `<td data-row="${row}" data-col="${col}"></td>`);
+    },
+};
+
+game.init();
